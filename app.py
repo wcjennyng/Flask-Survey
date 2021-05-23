2921a2b95328ebd4ed0316d14c401140bb904b0f
Jenny Ng <wcjennyng@gmail.com>
2021-05-23 17:27:06 -0400

    step 8, adding Flask session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = 'secret_name'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+standard = 'responses'
 
 @app.route('/')
 def home_page():
@@ -17,12 +17,16 @@ def home_page():
 
 @app.route('/start', methods=['POST'])
 def start():
-    """Begins survey/refreshes survey"""
+    """Begins survey"""
+    session[standard] = []
     return redirect('/questions/0')
 
 @app.route('/questions/<int:num>')
 def question_pg(num):
     """Show question"""
+
+    responses = session.get(standard)
+
     if (len(responses) == len(survey.questions)):
         return redirect('/completed')
 
@@ -37,7 +41,11 @@ def question_pg(num):
 def response():
     """Answer and redirect to next question"""
     choice = request.form['response']
+
+    #add to session
+    responses = session[standard]
     responses.append(choice)
+    session[standard] = responses
 
     if (len(responses) == len(survey.questions)):
         return redirect('/completed')
